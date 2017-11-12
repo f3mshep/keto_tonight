5a23f2a6c831cb6b0e9d0a9fd5d168fff5af913b
Alexandra <github email address>
2017-11-12 13:36:13 -0800

    working on categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,16 @@
 class CategoriesController < ApplicationController
 
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @recipes =
+  end
+
+  def destroy
+    @category =
+    authorize @category
+  end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ApplicationRecord
   has_many :recipe_categories
   has_many :recipes, through: :recipe_categories
   validates :name, uniqueness: true
+
+  def show
+    @recipes = Recipe
+  end
+
 end
